e01a172c2aa10880566dff92717319f3053d6fb8
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2024-11-05 15:32:41 +0300

    created base model list template and routed

--- cars/carshop/urls.py
+++ cars/carshop/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('brand-list/', views.BrandList.as_view(), name='brand_list'),
     path('add-brand/', views.AddBrand.as_view(), name='add_brand'),
     path('edit/<slug:brand_slug>', views.UpdateBrand.as_view(), name='edit_brand'),
+    path('brand/<slug:brand_slug>/model-list/', views.ModelList.as_view(), name='model_list'),
+    path('brand/<slug:brand_slug>/model-list/add_model', views.AddModel.as_view(), name='add_model'),
 ]
--- cars/carshop/views.py
+++ cars/carshop/views.py
@@ -1,7 +1,8 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Car, Brand
+from .models import Car, Brand, CarModel
 from django.urls import reverse_lazy
 # Create your views here.
 class IndexView(TemplateView):
@@ -20,6 +21,9 @@ class AddBrand(CreateView):
     template_name = 'carshop/add_brand.html'
     fields = ['name']
     success_url = reverse_lazy('brand_list')
+    extra_context = {
+        'view_name' : "add"
+    }
     
 class UpdateBrand(UpdateView):
     model = Brand
@@ -27,3 +31,20 @@ class UpdateBrand(UpdateView):
     fields = ['name']
     success_url = reverse_lazy('brand_list')
     slug_url_kwarg = 'brand_slug'
+    extra_context = {
+        'view_name' : "edit"
+    }
+
+class ModelList(ListView):
+    model = CarModel
+    template_name = 'carshop/model_list.html'
+    context_object_name = 'car_models'
+    extra_context = {
+        'add_model_link' : "add_model"
+    }
+
+class AddModel(CreateView):
+    model = CarModel
+    template_name = 'carshop/add_model.html'
+    fields = ['name', 'brand']
+    success_url = reverse_lazy('model_list')
