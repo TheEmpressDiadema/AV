87d4fba7177384cf129b8afefaa9c7910b4ed3eb
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2024-11-21 16:27:06 +0300

    Форма ввода множества фотографий

--- cars/carshop/forms.py
+++ cars/carshop/forms.py
@@ -1,13 +1,24 @@
 from django import forms
-from django.forms import inlineformset_factory, BaseInlineFormSet
-from django.core.exceptions import ValidationError
 from .models import Car
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('widget', MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = [single_file_clean(data, initial)]
+        return result
+
 class CarForm(forms.ModelForm):
-    images = forms.ImageField(
-        required=False,
-        label="Фотографии"
-    )
+    images = MultipleFileField(label="Изображения", required=False)
 
     class Meta:
         model = Car
